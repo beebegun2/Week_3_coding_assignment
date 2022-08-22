becec1d954094f8df921c13a2e484ffd34a0a107
Shannon Beebe <beebegun2.0@gmail.com>
2022-08-21 20:02:34 -0500

    working on class assignment

--- Week3.js
+++ Week3.js
@@ -57,16 +57,18 @@ average = names.join('').length / names.length
 console.log(average)
 }
 /*  b.	Use a loop to iterate through the array again and concatenate all the names together, separated by spaces, and print the result to the console.*/
+// use a loop to iterate thru the array again and concatenate all of the names together
+// then separate by spaces
+// print to console logs
+
 console.log(names);
 
-for (let i = 0; i < names.join; i++) {
+// loop concatenating all of the names together then separate with spaces ..instead of '' use ' '
+for (let i = 0; i < names.length; i++) {
 
-newNames = names.join("");
-              
-console.log(newNames);
+nameJoin = names.join(' ')
+console.log(nameJoin)
 }
-
-
 
 /*    
 3.	How do you access the last element of any array? - 
@@ -76,22 +78,49 @@ console.log(newNames);
     The first element is accessed using index 0 
 
 5.	Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
-*/
+
 //For example:
 
 namesArray = ["Kelly", "Sam", "Kate"] //given this array
 nameLengths = [5, 3, 4] //create this new array
 
+for (let i = 0; i < namesArray; i++) {
+    */
+
+let namesArray = ["Kelly", "Sam", "Kate"];
+let nameLengths = namesArray.map(function(element){
+    return element.length;
+});
+console.log(nameLengths);
 
 /*
 6.	Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array. Print the result to the console.
+
 7.	Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’).
+
 8.	Write a function that takes two parameters, firstName and lastName, and returns a full name (the full name should be the first and the last name separated by a space).
+*/
+
+function fullName(firstName, lastName){
+    return firstName + " " + lastName;
+  }
+  
+  //calling the function and assigning the return to a variable
+  var fullname = fullName("Shannon", "Beebe");
+  
+  console.log(fullname)
+
+/*
 9.	Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
+
 10.	Write a function that takes an array of numbers and returns the average of all the elements in the array.
+
 11.	Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+
 12.	Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+
 13.	Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
+
 Screenshots of Code:
 
 Screenshots of Running Application:
